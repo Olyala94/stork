2e162646773c89029974ad57813bb27f6e933289
Olyala94 <yusupowa9494@mail.ru>
2024-06-29 15:21:35 +0300

    page gome.blade oluşturulması

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,83 @@
+<footer id="footer">
+	<div class="container">
+		<div class="footer-content">
+			<div class="footer-menu social-media">
+				<ul>
+					<li><a href="#">facebook</a></li>
+					<li><a href="#">twitter</a></li>
+					<li><a href="#">instagram</a></li>
+				</ul>
+			</div>
+
+			<div class="footer-menu">
+				<ul>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Policy</a></li>
+					<li><a href="#">Term</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+
+			<div class="footer-menu">
+				<ul>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Policy</a></li>
+					<li><a href="#">Term</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+
+			<div class="footer-menu">
+				<ul>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Policy</a></li>
+					<li><a href="#">Term</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+
+			<div class="footer-menu">
+				<ul class="address">
+					<li><a href="#">contact@yourcompany.com</a></li>
+					<li>15Th Street Avenue, New York, USA</li>
+					<li>011-554-8798-6556</li>
+				</ul>
+				<div class="social-links">
+					<ul>
+						<li>
+							<a href="#">
+								<i class="icon icon-facebook"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#"><i class="icon icon-twitter"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#"><i class="icon icon-instagram"></i>
+							</a>
+						</li>
+						<li>
+							<a href="#"><i class="icon icon-skype"></i>
+							</a>
+						</li>
+					</ul>
+				</div><!--social-links-->
+			</div>
+		</div><!--footer-content-->
+		<div class="footer-bottom">
+			<div class="col-md-6">
+				<p>© Copyright 2023 LifeStyle. All rights reserved. </p>
+			</div>
+			<div class="col-md-6 text-start text-md-end">
+				<p>Free HTML Template by <a href="https://templatesjungle.com" target="_blank">TemplatesJungle</a> Distribution By <a href="https://themewagon.com">ThemeWagon</a></p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<script src="js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ЭКСПРЕСС УБОРКА ЛЕГКО И БЫСТРО</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="author" content="TemplatesJungle">
+    <meta name="keywords" content="magazine, blog, lifestyle">
+    <meta name="description" content="HTML Template for Magazine and LifeStyle Bloggers">
+
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="icomoon/icomoon.css">
+    <link rel="stylesheet" type="text/css" href="fonts/fonts.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <!-- script
+ ================================================== -->
+    <script src="js/modernizr.js"></script>
+
+</head>
+
+<body>
+
+    <div id="entire-wrapper">
+        <header id="header">
+            <div class="container">
+                <div class="row">
+                    <div class="header-wrap">
+                        <div class="top-left">
+                            <div class="main-logo">
+                                <a href="#">
+                                    <img src="images/mainlogo.png" alt="logo">
+                                </a>
+                            </div><!--main-logo-->
+                            <div class="social-links">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="icon icon-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon icon-skype"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon icon-instagram"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon icon-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="icon icon-linkedin-with-circle"></i>
+                                        </a>
+                                    </li>
+
+                                </ul>
+                            </div><!--social-links-->
+                        </div><!--top-left-->
+                        <nav id="navbar" class="header-nav">
+                            <div class="header-menu">
+                                <ul class="nav-menu nav-hvr-effect">
+                                    <li class="nav-item">
+                                        <a href="index.html" class="nav-link" data-hover="Home">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#about-page" class="nav-link" data-hover="About">About</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#popular-stories" class="nav-link" data-hover="Stories">Stories</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#feat-post" class="nav-link" data-hover="Post">Post</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#health-tips" data-hover="Health" class="nav-link">Health</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#love-tips" data-hover="Love" class="nav-link">Love</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#featured-post" data-hover="Feat. Post" class="nav-link">feat. Post</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="https://templatesjungle.gumroad.com/l/lifestyle-magazine-html-template"
+                                            data-hover="GET PRO" class="nav-link"> GET PRO </a>
+                                    </li>
+                                </ul>
+
+                                <div class="hamburger">
+                                    <span class="bar"></span>
+                                    <span class="bar"></span>
+                                    <span class="bar"></span>
+                                </div>
+
+                            </div><!--header-menu-->
+                        </nav>
+                    </div><!--header-wrap-->
+                </div>
+            </div><!--container-->
+        </header>
+    </div><!--entire-wrapper-->
+
+</body>
+
+</html>
